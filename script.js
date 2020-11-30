bf2ab7ab97be6ed1a060055cb031ef17be3e0f8c
Mohammed849 <65894073+Mohammed849@users.noreply.github.com>
2020-11-30 13:32:07 +0100

    check

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 // check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())){
+    showSuccess(input);
+  }else{
+    showError(input, 'Email is not valid')
+  }
 }
 // check required field
 function chechRquired(inputArr){
@@ -30,19 +34,39 @@ function chechRquired(inputArr){
     }else{
       showSuccess(input);
     }
-    // console.log(input.value);
+// console.log(input.value);
   });
+}
+//check input length
+function checkLength(input, min, max) {
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  }else if(input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);  
+  }else{
+    showSuccess(input);
+  }
+}
+// check Passwords match
+function checkPasswordsMatch(input1, input2){
+  if(input1.value !== input2.value){
+    showError(input2, 'Passwords do not match');
+  }
 }
 // get field name
 function getFieldName(input){
   return input.id.charAt(0).toUpperCase()+input.id.slice(1);
 }
-// EventListener
+// Event Listener
 form.addEventListener('submit', function (e){
   e.preventDefault();
-  // console.log('submit');
-  // console.log(username.value);
+// console.log('submit');
+// console.log(username.value);
   chechRquired([username, email, password, password2]);
+  checkLength(username, 3, 20);
+  checkLength(password, 8, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
   // if (username.value === ''){
   //   showError(username,'username is required');
   // }else{
